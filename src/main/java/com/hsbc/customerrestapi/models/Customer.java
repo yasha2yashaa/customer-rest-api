63b1e725eb22ff6db0c564695baf4a9585ab0e3a
yasha2yashaa <yasha2yashaa@gmail.com>
2019-09-24 15:43:33 +0200

    Implemented Customer model

--- src/main/java/com/hsbc/customerrestapi/models/Customer.java
+++ src/main/java/com/hsbc/customerrestapi/models/Customer.java
@@ -1,4 +1,24 @@
 package com.hsbc.customerrestapi.models;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import lombok.Data;
+import org.hibernate.annotations.Cascade;
+
+import javax.persistence.*;
+
+@Data
+@Entity(name="customers")
 public class Customer {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", updatable = false, nullable = false)
+    private long id;
+
+    @Column(name = "name", updatable = false, nullable = false)
+    private String name;
+
+    @ManyToOne
+    @JsonIgnoreProperties({"sons"})
+    @Cascade(org.hibernate.annotations.CascadeType.ALL)
+    private Address address;
 }
