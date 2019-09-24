2aa5384955a9e75d35b1852f3a049971852d23e2
yasha2yashaa <yasha2yashaa@gmail.com>
2019-09-24 15:40:21 +0200

    Implemented Address model

--- src/main/java/com/hsbc/customerrestapi/models/Address.java
+++ src/main/java/com/hsbc/customerrestapi/models/Address.java
@@ -1,5 +1,29 @@
 package com.hsbc.customerrestapi.models;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import lombok.Data;
 
+import javax.persistence.*;
+import java.util.Set;
+
+@Data
+@Entity(name="addresses")
 public class Address {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", updatable = false, nullable = false)
+    private long id;
+
+    @Column(name = "city", updatable = false, nullable = false)
+    private String city;
+
+    @Column(name = "street", updatable = false, nullable = false)
+    private String street;
+
+    @Column(name = "ziCode", updatable = false, nullable = false)
+    private String zipCode;
+
+    @OneToMany(mappedBy = "father")
+    @JsonIgnoreProperties({"father"})
+    private Set<Customer> customers;
 }
