322eb0c78179c69864f8e57104ddb2d2ead55219
yasha2yashaa <yasha2yashaa@gmail.com>
2019-09-24 15:58:01 +0200

    Implemented CustomerService

--- src/main/java/com/hsbc/customerrestapi/services/CustomerService.java
+++ src/main/java/com/hsbc/customerrestapi/services/CustomerService.java
@@ -1,4 +1,26 @@
 package com.hsbc.customerrestapi.services;
 
-public class CustomerService implements Service{
+import com.hsbc.customerrestapi.models.Customer;
+import com.hsbc.customerrestapi.repositories.CustomerRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CustomerService implements RestService<Customer>{
+    private final CustomerRepository customerRepository;
+
+    @Autowired
+    public CustomerService(CustomerRepository customerRepository) {
+        this.customerRepository = customerRepository;
+    }
+
+    @Override
+    public void add(Customer customer) {
+        customerRepository.save(customer);
+    }
+
+    @Override
+    public Customer getById(long id) {
+        return customerRepository.findById(id).get();
+    }
 }
