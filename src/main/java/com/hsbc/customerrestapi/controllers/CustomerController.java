7f56c4bffe430b9dc865421ad5bac154a1ab9357
yasha2yashaa <yasha2yashaa@gmail.com>
2019-09-24 16:30:04 +0200

    Implemented CustomerController

--- src/main/java/com/hsbc/customerrestapi/controllers/CustomerController.java
+++ src/main/java/com/hsbc/customerrestapi/controllers/CustomerController.java
@@ -1,4 +1,31 @@
 package com.hsbc.customerrestapi.controllers;
 
-public class CustomerController {
+import com.hsbc.customerrestapi.models.Customer;
+import com.hsbc.customerrestapi.services.CustomerService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/customers")
+public class CustomerController implements Controller<Customer> {
+
+    private final CustomerService customerService;
+
+    @Autowired
+    public CustomerController(CustomerService customerService) {
+        this.customerService = customerService;
+    }
+
+    @Override
+    @RequestMapping(method = RequestMethod.POST, consumes = MediaType.APPLICATION_JSON_VALUE)
+    public void add(@RequestBody Customer customer) {
+        customerService.add(customer);
+    }
+
+    @Override
+    @RequestMapping(value = "/{id}", method = RequestMethod.GET)
+    public Customer getById(@PathVariable("id") Long id) {
+        return customerService.getById(id);
+    }
 }
