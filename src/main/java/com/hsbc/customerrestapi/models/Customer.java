2921a2747e1b501b9701a281206db7b5a3a4d2d7
yasha2yashaa <yasha2yashaa@gmail.com>
2019-09-25 09:37:25 +0200

    Removed @Data annotation and added getters and setters

--- src/main/java/com/hsbc/customerrestapi/models/Customer.java
+++ src/main/java/com/hsbc/customerrestapi/models/Customer.java
@@ -1,12 +1,10 @@
 package com.hsbc.customerrestapi.models;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
-import lombok.Data;
 import org.hibernate.annotations.Cascade;
 
 import javax.persistence.*;
 
-@Data
 @Entity(name="customers")
 public class Customer {
     @Id
@@ -27,7 +25,31 @@ public class Customer {
 
     // Constructor for testing
     public Customer(String name, Address address) {
+        this.setName(name);
+        this.setAddress(address);
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
         this.name = name;
+    }
+
+    public Address getAddress() {
+        return address;
+    }
+
+    public void setAddress(Address address) {
         this.address = address;
     }
 }
