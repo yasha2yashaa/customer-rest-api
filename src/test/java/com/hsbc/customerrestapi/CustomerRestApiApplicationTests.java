1ee3001f2a415f7d0603ff4cd411812f02c0617a
yasha2yashaa <yasha2yashaa@gmail.com>
2019-09-25 10:03:55 +0200

    Added 404 response test if customer does not exist

--- src/test/java/com/hsbc/customerrestapi/CustomerRestApiApplicationTests.java
+++ src/test/java/com/hsbc/customerrestapi/CustomerRestApiApplicationTests.java
@@ -1,9 +1,19 @@
 package com.hsbc.customerrestapi;
 
+import org.apache.http.HttpResponse;
+import org.apache.http.HttpStatus;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.methods.HttpUriRequest;
+import org.apache.http.impl.client.HttpClientBuilder;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import java.io.IOException;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.core.IsEqual.equalTo;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
@@ -11,6 +21,18 @@ public class CustomerRestApiApplicationTests {
 
 	@Test
 	public void contextLoads() {
+	}
+
+	@Test
+	public void whenCustomerDoesNotExistsThen404IsReceived() throws IOException {
+		// Given
+		HttpUriRequest request = new HttpGet( "localhost:8080/api/customers/1" );
+		// When
+		HttpResponse response = HttpClientBuilder.create().build().execute( request );
+		// Then
+		assertThat(
+				response.getStatusLine().getStatusCode(),
+				equalTo(HttpStatus.SC_NOT_FOUND));
 	}
 
 }
