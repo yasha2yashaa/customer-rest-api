049848ed250313df7865138997e5bb9b1e0dea70
yasha2yashaa <yasha2yashaa@gmail.com>
2019-09-26 10:48:28 +0200

    Changed getters and setters for @Data annotation

--- src/main/java/com/hsbc/customerrestapi/models/Customer.java
+++ src/main/java/com/hsbc/customerrestapi/models/Customer.java
@@ -20,37 +20,4 @@ public class Customer {
     @JsonIgnoreProperties({"customers", "id"})
     @Cascade(CascadeType.ALL)
     private Address address;
-
-    // Default Constructor
-    public Customer() {}
-
-    // Constructor for testing
-    public Customer(String name, Address address) {
-        this.setName(name);
-        this.setAddress(address);
-    }
-
-    public long getId() {
-        return id;
-    }
-
-    public void setId(long id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public Address getAddress() {
-        return address;
-    }
-
-    public void setAddress(Address address) {
-        this.address = address;
-    }
 }
